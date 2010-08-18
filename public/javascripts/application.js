bb157316f093a5dcb73c33b1efec1b9a809ac159
“Willy <will@rubify.com>
2010-08-18 11:16:38 +0800

    welcome page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-	
+	navControlBinder("#interesting", "#interesting-guides");
+	navControlBinder("#login", "#login-form");
+	navControlBinder("#register", "#registration-form");
 });
 
+
+function navControlBinder(nav_id, div_id){
+	$(nav_id).click(function(){
+		if($(this).hasClass('selected')){
+			
+		}else{
+			$("div.welcome-form").each(function(){
+				$(this).removeClass("current_show").hide();
+			});
+			$("a.nav-welcome").each(function(){
+				$(this).removeClass("selected");
+			});
+			$(div_id).addClass("current_show").show();
+			$(this).addClass('selected');
+		}
+		return false;
+	});
+}
